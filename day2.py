ff333cd0326a7a5fd8ab1181c04e01ea2270bd52
JAEHAN <dbswogks1534@naver.com>
2022-01-11 13:42:57 +0900

    f-string 사용

--- day2.py
+++ day2.py
@@ -3,5 +3,4 @@ total = float(input("what was the total bill? "))
 splits = int(input("How many people to split the bill? "))
 percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 total = total * (100 + percentage) / 100
-print("Each person should pay: $")
-print(round(total / splits, 1))
+print(f"Each person should pay: ${round(total / splits, 1)}")
